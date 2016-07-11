89f18b37a38bd3e906ddc9ab5fa2708e2d6d4483
yandai <yan.dai@intel.com>
2016-07-11 11:30:29 +0800

    Update GPU Control

--- gpu_control.py
+++ gpu_control.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
-
+import re
 import os
 from enum import Enum
+
+class GPUProcess(object):
+    """
+        Process Runing on  GPU
+    """
+
+    def __init__(self):
+        self.pid = None
+        self.command = None
+        self.sm_utilization = None
+        self.memory_utilization = None
+
+    def set_info(self, pid, command, sm_utilization, memory_utilization):
+        self.pid = pid
+        self.command = command
+        self.sm_utilization = sm_utilization
+        self.memory_utilization = memory_utilization
 
 
 class GPUDevice(object):
@@ -13,6 +30,8 @@ class GPUDevice(object):
             ...   
     """
     graphics_mode = Enum("INSTANT", "APPLICATION", "APPLICATION_DEFAULT", "MAX")
+    sm_free_threshold = 0.03
+    memory_free_threshold = 0.03
     
     def __init__(self, gpuid):
         """
@@ -20,13 +39,33 @@ class GPUDevice(object):
                 GPUID: gpu device N.O. as gpuid
         """
         self.gpuid = gpuid
-        pass
-   
+        self.processes = []
+         
+
     def sudo_wrapper(self, command):
         """
-            return sudo + command as strings
+            Returns:
+             sudo + command as strings
         """
         return "sudo " + command
+ 
+    def get_gpu_model(self):
+        """
+            get gpu model
+            Returns:
+                GPU Model Name 
+        """ 
+        command = "nvidia-smi -L"
+        fp = os.popen(command)
+        for line in fp:
+            line = line.strip()
+            m = re.match("GPU(.*)\:(.*)\(UUID.*\)", line)
+            gpuid = int(m.group(1))
+            gpu_model = m.group(2)
+            if gpuid == self.gpuid:
+                self.gpu_model = gpu_model
+                return self.gpu_model
+        return None
  
     def get_core_frequency(self, mode):
         """
@@ -116,8 +155,51 @@ class GPUDevice(object):
         return True if self.get_autoboost() == mode else False
         
 
+    def get_utlization(self):
+        """
+            get utilization
+            Args:
+                Mode: int, 1 as autoboost on and 0 as autoboost off
+            Returns:
+                True when successfully setting the clocks  
+        """
+        core_command = "nvidia-smi -q -d utilization -i %d | grep Gpu | awk '{print $(NF-1)}'" % (self.gpuid, )
+        fp = os.popen(self.sudo_wrapper(core_command))
+        core_utilization = float(0.01 * int(fp.read()))
+        mem_command = "nvidia-smi -q -d utilization -i %d | grep Memory | grep -vi sam | awk '{print $(NF-1)}'" % (self.gpuid, )
+        fp = os.popen(self.sudo_wrapper(mem_command))
+        mem_utilization = float(0.01 * int(fp.read()))
+        return (core_utilization, mem_utilization)
+
+    def is_gpu_free(self):
+        """
+            judge whether GPU is free or not
+            Returns:
+                True when GPU is free        
+        """
+        (core_utilization, mem_utilization) = self.get_utlization()
+        if core_utilization < self.__class__.sm_free_threshold and \
+            mem_utilization < self.__class__.memory_free_threshold:
+            return True
+        else:
+            return False
+
     def get_running_process(self):
-        pass    
+        """
+            get information of GPU running process
+        """
+        command = "nvidia-smi pmon -i %d --count 1 | grep -v '^#'" % (self.gpuid, )
+        fp = os.popen(self.sudo_wrapper(command))
+        self.processes = []
+        for line in fp:
+            line = line.strip()
+            info = re.split(' +', line)[0:8]
+            (gpu, pid, proces_type, sm, mem, enc, dec, command) = info             
+            gp = GPUProcess()
+            gp.set_info(pid, command, sm, mem)
+            self.processes.append(gp)
+        return self.processes 
+    
 
 if __name__ == '__main__':
     gpu = GPUDevice(0)
@@ -128,3 +210,7 @@ if __name__ == '__main__':
     print(gpu.get_memory_frequency(graphics_mode.INSTANT))
     print(gpu.set_autoboost(1))
     print(gpu.get_autoboost())
+    print(gpu.get_utlization())
+    print(gpu.is_gpu_free())
+    print(gpu.get_running_process())
+    gpu.get_gpu_model()
