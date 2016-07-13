35caa7ac114f3b2dc1d247c8e5042eab5b04005d
yandai <yan.dai@intel.com>
2016-07-13 23:37:09 +0800

    small modification in tornado application

--- server/server_start.py
+++ server/server_start.py
@@ -3,18 +3,29 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
-result_content_html = "template/result.html"
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
-class IndexHandler(tornado.web.RequestHandler):
+
+class TestRequest(tornado.web.RequestHandler):
+    test_request_html = "template/test_request.html"
+
     def get(self):
-        self.render("../" + result_content_html)
+        self.render(self.__class__.test_request_html)
+
+    def post(self):
+        topology = self.get_argument('topology')
+        print(topology)        
+        self.write("received!")
+
+
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers = [(r"/", IndexHandler)])
+    app = tornado.web.Application(handlers = [(r"/request", TestRequest), \
+        (r'/css/(.*)', tornado.web.StaticFileHandler, {'path': 'template/css'})
+    ])
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
