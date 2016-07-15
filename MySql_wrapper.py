070f9d9c9726895b63bacb4a1eb8fc8c1b470192
zzrgo <zzr.631168309@gmail.com>
2016-07-15 17:51:35 +0800

    fix wrong database bug

--- MySql_wrapper.py
+++ MySql_wrapper.py
@@ -9,7 +9,7 @@ class Mysql_wrapper():
 
     def __init__(self, host, user, passwd):
         self.connection = \
-            MySQLdb.Connect(host="%s" % (host,), user="%s" % (user,), passwd="%s" % (passwd,), db="%s" % ("mysql",))
+            MySQLdb.Connect(host="%s" % (host,), user="%s" % (user,), passwd="%s" % (passwd,), db="%s" % ("automation",))
 
     def __del__(self):
         self.connection.close()
@@ -23,28 +23,28 @@ class Mysql_wrapper():
         Returns:
         """
         cursor = self.connection.cursor()
-        create_database = "CREATE DATABASE IF NOT EXISTS animations;"
-        use_database = "USE animations;"
-        create_table  = "CREATE TABLE IF NOT EXISTS docker_images (id INT NOT NULL AUTO_INCREMENT, REPOSITORY VARCHAR(500) NOT NULL, TAG VARCHAR(500) NOT NULL,CUDA_VERSION FLOAT NOT NULL,CUDA_VERSION_STRING VARCHAR(500) NOT NULL,CUDNN_VERSION FLOAT NOT NULL,CUDNN_VERSION_STRING VARCHAR(500) NOT NULL,TENSORFLOW BOOL NOT NULL, CAFFE BOOL NOT NULL,PRIMARY KEY (id));"
-        sql_cmd = use_database + create_table
-        farmer_log.info("init database command : [%s]" % create_database)
-        cursor.execute(create_database)
-        farmer_log.info("init table : [%s]" % sql_cmd)
-        cursor.execute(sql_cmd)
-        farmer_log.debug("rowcount : [%d]" % cursor.rowcount)
-        if -1 != cursor.rowcount:
-            result = cursor.fetchall()
-            farmer_log.info(result)
+        create_docker_images_table_cmd  = "CREATE TABLE IF NOT EXISTS docker_images (id INT NOT NULL AUTO_INCREMENT, REPOSITORY VARCHAR(500) NOT NULL, TAG VARCHAR(500) NOT NULL,CUDA_VERSION FLOAT NOT NULL,CUDA_VERSION_STRING VARCHAR(500) NOT NULL,CUDNN_VERSION FLOAT NOT NULL,CUDNN_VERSION_STRING VARCHAR(500) NOT NULL,TENSORFLOW BOOL NOT NULL, CAFFE BOOL NOT NULL,PRIMARY KEY (id));"
+        create_result_report_table_cmd = "CREATE TABLE IF NOT EXISTS result_reports (id INT NOT NULL AUTO_INCREMENT, RESQUEST_ID INT NOT NULL, DOCKER_ID INT NOT NULL, GPU_MODULE VARCHAR(500) NOT NULL, MAIL_ADDRESS VARCHAR(500) NOT NULL, FRAMEWORK VARCHAR(500) NOT NULL, TOPOLOGY VARCHAR(500) NOT NULL, BATCH_SIZE INT NOT NULL, ITERATION INT NOT NULL, PRIMARY KEY (id));"
+
+        # execute initalizing the docker_images command
+        farmer_log.info("init docker_images table : [%s]" % create_docker_images_table_cmd)
+        cursor.execute(create_docker_images_table_cmd)
+        farmer_log.info(cursor.fetchall())
+        self.connection.commit()
+
+        # execute initalizing the result_reports command
+        farmer_log.info("init result_reports table : [%s]" % create_result_report_table_cmd)
+        cursor.execute(create_result_report_table_cmd)
+        farmer_log.info(cursor.fetchall())
+
         cursor.close()
 
     def has_image_in_db_by_rep_tag(self, repository, tag):
         result = False
         cursor = self.connection.cursor()
         try:
-            use_table = "USE animations;"
             seach_image = "SELECT repository, tag FROM docker_images WHERE REPOSITORY = '%s' AND TAG = '%s';" % (repository, tag)
-            sql_command = use_table + seach_image
-            farmer_log.debug(sql_command)
+            farmer_log.debug(seach_image)
             cursor.execute(seach_image)
             farmer_log.debug(cursor.rowcount)
             if (-1 != cursor.rowcount) and (0 != cursor.rowcount):
@@ -61,15 +61,13 @@ class Mysql_wrapper():
     def inert_item_in_docker_images_table(self, repository, tag, cuda_version, cuda_version_str, cudnn_version, cudnn_version_str, have_tensorflow, have_caffe):
         cursor = self.connection.cursor()
         try:
-            use_table = "USE animations;"
             inserted_sql = 'INSERT INTO docker_images (REPOSITORY, TAG,\
              CUDA_VERSION, CUDA_VERSION_STRING, CUDNN_VERSION,\
              CUDNN_VERSION_STRING, TENSORFLOW, CAFFE) \
              VALUES("%s", "%s", %f, "%s", %f, "%s", %d, %d);' % \
               (repository, tag, cuda_version, cuda_version_str, cudnn_version,\
                cudnn_version_str, have_tensorflow, have_caffe)
-            sql_command = use_table + inserted_sql
-            farmer_log.debug(sql_command)
+            farmer_log.debug(inserted_sql)
             cursor.execute(inserted_sql)
             self.connection.commit()
             output = cursor.fetchall()
@@ -80,14 +78,29 @@ class Mysql_wrapper():
         finally:
             cursor.close()
 
+    def inert_item_in_result_reports(self, resquest_id, docker_id, gpu_module, mail_addr, framework, topology, batch_size, iteration):
+        cursor = self.connection.cursor()
+        try:
+            inserted_sql = 'INSERT INTO result_reports\
+            (RESQUEST_ID,  DOCKER_ID,  GPU_MODULE,  MAIL_ADDRESS,  FRAMEWORK, TOPOLOGY,  BATCH_SIZE,  ITERATION) \
+      VALUES(%d,           %d,         "%s",        "%s",          "%s",      "%s",      %d,          %d);' % \
+            (resquest_id,  docker_id,  gpu_module,  mail_addr,     framework, topology,  batch_size,  iteration)
+            cursor.execute(inserted_sql)
+            self.connection.commit()
+            output = cursor.fetchall()
+            farmer_log.info(output)
+        except Exception as e:
+            self.connection.rollback()
+            farmer_log.error("inert_item_in_result_report:" + e.message)
+        finally:
+            cursor.close()
+
     def has_image_in_db_by_cuda_cuddn(self, cuda_string, cuddn_string):
         result = False
         cursor = self.connection.cursor()
         try:
-            use_table = "USE animations;"
             seach_image = "SELECT repository, tag FROM docker_images WHERE CUDA_VERSION_STRING = '%s' AND CUDNN_VERSION_STRING = '%s';" % (cuda_string, cuddn_string)
-            sql_command = use_table + seach_image
-            farmer_log.debug(sql_command)
+            farmer_log.debug(seach_image)
             cursor.execute(seach_image)
             farmer_log.debug(cursor.rowcount)
             if (-1 != cursor.rowcount) and (0 != cursor.rowcount):
@@ -105,10 +118,7 @@ class Mysql_wrapper():
         row = None
         cursor = self.connection.cursor()
         try:
-            use_table = "USE animations;"
             select_sql = "SELECT * FROM docker_images WHERE REPOSITORY = '%s' AND TAG = '%s';" % (repository, tag)
-            farmer_log.info(use_table)
-            cursor.execute(use_table)
             farmer_log.info(select_sql)
             cursor.execute(select_sql)
             numrows = int(cursor.rowcount)
@@ -129,8 +139,9 @@ class Mysql_wrapper():
 if __name__ == "__main__":
     wrapper = Mysql_wrapper("localhost", "root", "RACQ4F6c")
     wrapper.init_database()
-    #wrapper.inert_item_in_docker_images_table("nvidia/cuda", "7.5-cudnn5.1rc-devel-ubuntu14.04-caffe-pcs", 7.5,
-    #                                         "cuda_7.5.18", 5.0, "cudnn-7.5-linux-x64-v5.0-rc", False, True)
+    wrapper.inert_item_in_docker_images_table("nvidia/cuda", "7.5-cudnn5.1rc-devel-ubuntu14.04-caffe-pcs", 7.5,\
+                                            "cuda_7.5.18", 5.0, "cudnn-7.5-linux-x64-v5.0-rc", False, True)
+   # wrapper.inert_item_in_result_reports(1, 1, "GPU_MODULE", "a@c.com", "framework", "topology", 0, 100)
     print wrapper.get_detailed_info_from_db("nvidia/cuda", "7.5-cudnn5.1rc-devel-ubuntu14.04-caffe-pcs")
     #print wrapper.has_image_in_db_by_cuda_cuddn("cuda_7.5.18", "cudnn-7.5-linux-x64-v5.0-rc")
 
